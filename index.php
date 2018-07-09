24617a5af8bb985288a50dc57fd7198da8e457c0
dhruv-dj <dhruv2703@gmail.com>
2018-07-09 19:42:34 +0530

    php

--- index.php
+++ index.php
@@ -545,7 +545,7 @@ We offer lavish and luxurious decor for our clients to receive their guests and 
             <div class="social-links">
 <!--              <a href="#" class="twitter"><i class="fab fa-twitter"></i></a>-->
               <a href="https://www.facebook.com/decoravantgarde" class="facebook"><i class="fab fa-facebook"></i></a>
-              <a href="https://www.instagram.com/decoravantgarde/" class="instagram"><i class="fab fa-instagram"></i></a>
+              <a href="https://www.instagram.com/decoreavantgarde/" class="instagram"><i class="fab fa-instagram"></i></a>
 <!--              <a href="#" class="google-plus"><i class="fab fa-google-plus"></i></a>-->
 <!--              <a href="#" class="linkedin"><i class="fab fa-linkedin"></i></a>-->
             </div>
